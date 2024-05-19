db67f0732cf2a10624fad4c2bfb8e9a5d6ad552c
kersha0530 <168761614+kersha0530@users.noreply.github.com>
2024-05-19 18:30:42 -0500

    Create Kershab_projsetup.py

---
+++ Kershab_projsetup.py
@@ -0,0 +1,56 @@
+''' This module provides functions for creating a series of project folders. '''
+import math
+import statistics
+import stellar_analytics_utils
+  def create_folders_for_range(start, end):
+  def create_folders_from_list(folder_list): 
+  def create_prefixed_folders(folder_list, prefix):
+  def create_folders_periodically(duration):
+    import pathlib
+
+# Create a path object
+project_path = pathlib.Path.cwd()
+
+# Define the new sub folder path
+data_path = project_path.joinpath('data')
+
+# Create new if it doesn't exist, otherwise do nothing
+data_path.mkdir(exist_ok=True)
+def main():
+    ''' Main function to demonstrate module capabilities. '''
+
+    # Print byline from imported module
+    print(f"Byline: {stellar_analytics_utils.byline}")
+
+    # Call function 1 to create folders for a range (e.g. years)
+    create_folders_for_range(start_year=2020, end_year=2023)
+
+    # Call function 2 to create folders given a list
+    folder_names = ['data-csv', 'data-excel', 'data-json']
+    create_folders_from_list(folder_names)
+
+    # Call function 3 to create folders using comprehension
+    folder_names = ['csv', 'excel', 'json']
+    prefix = 'data-'
+    create_prefixed_folders(folder_names, prefix)
+
+    # Call function 4 to create folders periodically using while
+    duration_secs = 5  # duration in seconds
+    create_folders_periodically(duration_secs)
+
+    # Add options e.g., to force lowercase and remove spaces 
+    # to one or more of your functions (e.g. function 2) 
+    # Call your function and test these options
+    regions = [
+      "North America", 
+      "South America", 
+      "Europe", 
+      "Asia", 
+      "Africa", 
+      "Oceania", 
+      "Middle East"
+    ]
+    create_folders_from_list(regions, to_lowercase=True, remove_spaces=True)
+  if __name__ == '__main__':
+    main()
+    
